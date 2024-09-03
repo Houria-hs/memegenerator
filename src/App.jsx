60ae92de9c7c498101feedea27abb22128c91a8c
Houria <126777373+Hou-ry@users.noreply.github.com>
2024-09-03 21:50:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./header"
+import Meme from "./meme"
+
+export default function App() {
+    console.log("hey")
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
